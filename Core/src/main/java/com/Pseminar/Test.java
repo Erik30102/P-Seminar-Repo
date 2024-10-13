ab8751f2ff0e091a00dcb1c8d2fb28141b22ef81
Erik <57637415+Erik30102@users.noreply.github.com>
2024-10-13 20:34:45 +0200

    Setup General Project Sturctur

---
+++ Core/src/main/java/com/Pseminar/Test.java
@@ -0,0 +1,7 @@
+package com.Pseminar;
+
+public class Test {
+    public static String TestTest() {
+        return "Das is ne TEst";
+    }
+}
---
+++ Sandbox/src/main/java/com/Sandbox/main.java
@@ -0,0 +1,9 @@
+package com.Sandbox;
+
+import com.Pseminar.Test;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(Test.TestTest());
+    }
+}
