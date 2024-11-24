7ebcabbc6bea9e2f34fc0c062f3ef9c07e535d5e
Jonas <jh@kaitan.de>
2024-11-24 09:30:39 +0000

    basic shaders (noch nicht ganz fertig)

--- Core/src/main/java/com/Pseminar/Graphics/Shaders/vertex_shader.glsl
+++ Core/src/main/java/com/Pseminar/Graphics/Shaders/vertex_shader.glsl
@@ -3,9 +3,10 @@
 layout(location = 0) in vec3 position; // Vertex position input
 layout(location = 1) in vec3 color;    // Vertex color input
 
+
 out vec3 fragColor; // Output color to fragment shader
 
 void main() {
-    gl_Position = vec4(position, 1.0); // Set the position of the vertex
     fragColor = color;                 // Pass the color to the fragment shader
+    gl_Position = vec4(position, 1.0); // Set the position of the vertex
 }
