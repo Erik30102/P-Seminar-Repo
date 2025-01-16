2dcba5b1a003ae253baacae57755918647590be7
Danielswl <daniel.schreiber@mailbox.org>
2025-01-16 18:49:49 +0100

    Correct Framebuffer.java

--- Core/src/main/java/com/Pseminar/Graphics/Buffers/Framebuffer.java
+++ Core/src/main/java/com/Pseminar/Graphics/Buffers/Framebuffer.java
@@ -1,34 +1,46 @@
 import java.awt.image.BufferedImage;
 import java.awt.Color;
 
+public class FrameBuffer {
+    private int width;
+    private int height;
+    private BufferedImage buffer;
 
-public class Framebuffer{
-  private int width;
-  private int height;
-  private BufferedImage buffer;
-
-  public Framebuffer(int width, int height) {
-    this.width = width;
-    this.height = height;
-    this.buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-  }
-
-
-  public void setPixel(int x, int y, Color color) {
-    if (int y = 0 && x < width && y >== 0 && y < height) {
-      buffer.setRGB(x,y, color.getRGB());
+    public FrameBuffer(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
     }
-  }
 
-
-public void clear(Color color) {
-  for (int y = 0; y < height; y++){
-    for (int x = 0; x < width, x++){
-      buffer.setRGB(x,y, color.getRGB());
+    /**
+     * Sets the color of a specific pixel in the framebuffer.
+     * @param x The x coordinate of the pixel.
+     * @param y The y coordinate of the pixel.
+     * @param color The color to set the pixel.
+     */
+    public void setPixel(int x, int y, Color color) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
+            buffer.setRGB(x, y, color.getRGB());
+        }
     }
-  }
-}
-public BufferedImage getBuffer() {
-  return buffer;
+
+    /**
+     * Clears the framebuffer with a specified color.
+     * @param color The color to clear the framebuffer with.
+     */
+    public void clear(Color color) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                buffer.setRGB(x, y, color.getRGB());
+            }
+        }
+    }
+
+    /**
+     * Returns the BufferedImage representing the framebuffer.
+     * @return The BufferedImage of the framebuffer.
+     */
+    public BufferedImage getBuffer() {
+        return buffer;
+    }
 }
