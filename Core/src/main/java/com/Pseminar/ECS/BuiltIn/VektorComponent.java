6e64d97213ac9bb5880926a8276030fba3d96bf0
Dani <daniel@leupold-serrano.de>
2025-02-02 12:00:25 +0100

    Vektor Component von vor Weihnachten erstellt

---
+++ Core/src/main/java/com/Pseminar/ECS/BuiltIn/VektorComponent.java
@@ -0,0 +1,36 @@
+package com.Pseminar.ECS.BuiltIn;
+
+import com.Pseminar.ECS.Component;
+
+public class VektorComponent implements Component{
+    
+    private int x = 0;
+    private int y = 0;
+
+    void MoveX(int amount)
+    {
+        this.x += amount;
+    }
+
+    void MoveY(int amount)
+    {
+        this.y += amount;
+    }
+
+    void Move(int x_amount, int y_amount)
+    {
+        this.x += x_amount;
+        this.y += y_amount;
+    }
+
+    int GiveX()
+    {
+        return this.x;
+    }
+
+    int GiveY()
+    {
+        return this.y;
+    }
+
+}
