0da2fad53c79d3978220ef27f527ac6d68461cb1
Danielswl <daniel.schreiber@mailbox.org>
2025-03-12 22:16:01 +0100

    Implement MouseEvent.java

--- Core/src/main/java/com/Pseminar/Window/Events/InputEvents/MouseEvent.java
+++ Core/src/main/java/com/Pseminar/Window/Events/InputEvents/MouseEvent.java
@@ -1,5 +1,19 @@
 package com.Pseminar.Window.Events.InputEvents;
 
-public class MouseEvent {
+public class MouseEvent extends Event {
+    private final int x, y;
 
+    public MouseEvent(EventType type, int x, int y) {
+        super(type);
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    @Override
+    public String toString() {
+        return "MouseEvent{" + "type=" + getType() + ", x=" + x + ", y=" + y + '}';
+    }
 }
