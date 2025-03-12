3461a9598f9cdaa1c70e8471aea7f8bb5b38c247
Danielswl <daniel.schreiber@mailbox.org>
2025-03-12 22:18:58 +0100

    Implement ScrollEvent.java

--- Core/src/main/java/com/Pseminar/Window/Events/InputEvents/ScrollEvent.java
+++ Core/src/main/java/com/Pseminar/Window/Events/InputEvents/ScrollEvent.java
@@ -1,5 +1,17 @@
 package com.Pseminar.Window.Events.InputEvents;
 
-public class ScrollEvent {
+public class ScrollEvent extends Event {
+ private final float scrollAmount;
 
+    public ScrollEvent(float scrollAmount) {
+        super(EventType.MOUSE_SCROLLED);
+        this.scrollAmount = scrollAmount;
+    }
+
+    public float getScrollAmount() { return scrollAmount; }
+
+    @Override
+    public String toString() {
+        return "ScrollEvent{" + "scrollAmount=" + scrollAmount + '}';
+    }
 }
