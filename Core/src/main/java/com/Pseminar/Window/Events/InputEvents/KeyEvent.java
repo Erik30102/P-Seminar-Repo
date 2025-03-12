f16413f4c574378c4373c57009d837b74969a6fe
Danielswl <daniel.schreiber@mailbox.org>
2025-03-12 21:47:29 +0100

    implement KeyEvent.java

--- Core/src/main/java/com/Pseminar/Window/Events/InputEvents/KeyEvent.java
+++ Core/src/main/java/com/Pseminar/Window/Events/InputEvents/KeyEvent.java
@@ -1,5 +1,19 @@
 package com.Pseminar.Window.Events.InputEvents;
 
-public class KeyEvent {
+public class KeyEvent extends Event {
+ private final int keyCode;
 
+    public KeyEvent(EventType type, int keyCode) {
+        super(type);
+        this.keyCode = keyCode;
+    }
+
+    public int getKeyCode() {
+        return keyCode;
+    }
+
+    @Override
+    public String toString() {
+        return "KeyEvent{" + "type=" + getType() + ", keyCode=" + keyCode + '}';
+    }
 }
