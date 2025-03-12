8fa5524ae0098b6fe9110ce6bafc00c6dc7615ef
Danielswl <daniel.schreiber@mailbox.org>
2025-03-12 21:59:23 +0100

    implement MouseClickEvent.java

--- Core/src/main/java/com/Pseminar/Window/Events/InputEvents/MouseClickEvent.java
+++ Core/src/main/java/com/Pseminar/Window/Events/InputEvents/MouseClickEvent.java
@@ -1,5 +1,21 @@
 package com.Pseminar.Window.Events.InputEvents;
 
-public class MouseClickEvent {
+public class MouseClickEvent extends Event {
+    private final int x, y, button;
 
+    public MouseClickEvent(int x, int y, int button) {
+        super(EventType.MOUSE_CLICKED);
+        this.x = x;
+        this.y = y;
+        this.button = button;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getButton() { return button; }
+
+    @Override
+    public String toString() {
+        return "MouseClickEvent{" + "x=" + x + ", y=" + y + ", button=" + button + '}';
+    }
 }
